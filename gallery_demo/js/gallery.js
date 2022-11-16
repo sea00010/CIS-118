330a1d07d34c8a3e9c900d4854383dd56b07a610
sea00010 <112178902+sea00010@users.noreply.github.com>
2022-11-16 08:39:19 -0500

    Add files via upload

---
+++ gallery_demo/js/gallery.js
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<meta name=”author” content=”Linda Celet Bane”>
+		<title>Photo Gallery</title>
+		<!--Later we will put the css and js links
+		<link rel="stylesheet" href="css/gallery.css">
+		<script src="js/gallery.js"></script>
+		In this spot.--> 
+
+		<style>
+		</style>
+	</head>
+<body>
+		<h1>Creating a Photo Gallery with CSS & JavaScript</h1>
+		<footer role="contentinfo">
+		This gallery was created using the YouTube tutorial: <a href="https://youtu.be/sHG3Sf6XN9g">How To Make Image Gallery Using HTML, CSS & JavaScript</a>
+		</footer>
+</body>
+</html>
