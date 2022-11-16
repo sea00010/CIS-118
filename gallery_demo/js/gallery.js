d429ea8d631515640c6692cbc400b715a8831c9a
sea00010 <112178902+sea00010@users.noreply.github.com>
2022-11-16 09:10:43 -0500

    Update gallery.js

--- gallery_demo/js/gallery.js
+++ gallery_demo/js/gallery.js
@@ -1,21 +1,9 @@
-<!doctype html>
-<html lang="en-us">
-	<head>
-		<meta charset="utf-8">
-		<meta name=”author” content=”Linda Celet Bane”>
-		<title>Photo Gallery</title>
-		<!--Later we will put the css and js links
-		<link rel="stylesheet" href="css/gallery.css">
-		<script src="js/gallery.js"></script>
-		In this spot.--> 
-
-		<style>
-		</style>
-	</head>
-<body>
-		<h1>Creating a Photo Gallery with CSS & JavaScript</h1>
-		<footer role="contentinfo">
-		This gallery was created using the YouTube tutorial: <a href="https://youtu.be/sHG3Sf6XN9g">How To Make Image Gallery Using HTML, CSS & JavaScript</a>
-		</footer>
-</body>
-</html>
+var fullImgBox = document.getElementById("fullImgBox");
+		var fullImg = document.getElementById("fullImg");
+		function openFullImg(pic){
+			fullImgBox.style.display = "flex";
+			fullImg.src = pic;
+		}
+		function closeFullImg(){
+			fullImgBox.style.display = "None";
+		}
